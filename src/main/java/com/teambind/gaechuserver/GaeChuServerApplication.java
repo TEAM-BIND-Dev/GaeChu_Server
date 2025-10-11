674695bd345f3eace3dcda6639a7333195ccf6f2
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-11 10:10:08 +0900

    Initial commit

---
+++ src/main/java/com/teambind/gaechuserver/GaeChuServerApplication.java
@@ -0,0 +1,13 @@
+package com.teambind.gaechuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaeChuServerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GaeChuServerApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/teambind/gaechuserver/GaeChuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teambind.gaechuserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaeChuServerApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
