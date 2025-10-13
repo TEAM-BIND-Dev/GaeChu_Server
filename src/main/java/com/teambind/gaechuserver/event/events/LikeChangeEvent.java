d2afaefaf13e6a8430d44409fcc90bd603a05987
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-13 16:51:00 +0900

    [FEAT] 좋아요 관련 테스트 케이스 및 설정 추가
    src/test/java/com/teambind/gaechuserver/utils/DataInitializerTest.java: 데이터 초기화 및 카테고리 캐싱 로직 테스트 추가
    src/main/java/com/teambind/gaechuserver/event/publisher/EventPublisher.java: Kafka 기반 이벤트 퍼블리셔 구현 추가
    src/test/java/com/teambind/gaechuserver/controller/LikeControllerWebMvcTest.java: 좋아요 컨트롤러 웹 MVC 테스트 추가
    src/test/java/com/teambind/gaechuserver/integration/LikeReadIntegrationTest.java: 좋아요 조회 통합 테스트 추가(Data JPA 사용)
    src/test/java/com/teambind/gaechuserver/service/LikeReadServiceTest.java: 카테고리별 좋아요 조회 서비스 테스트 추가
    .env, .env.prod: 데이터베이스 이름 `likes` → `gaechus` 변경
    Docker-compose.yml: 다중 서버 구성 및 헬스체크/로그 설정 추가
    Dockerfile: Spring Boot JAR 빌드되는 Dockerfile 작성
    ReadMe.md: Like API 문서 및 사용 예시 추가
    nginx/config/default.conf: Nginx 프록시 Config 예제 추가(build용)
    build.gradle: 테스트 의존성으로 H2 Database 추가
    
    참고: ReadMe, Docker 설정 적용 테스트 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/gaechuserver/event/events/LikeChangeEvent.java
+++ src/main/java/com/teambind/gaechuserver/event/events/LikeChangeEvent.java
@@ -1,17 +1,11 @@
 package com.teambind.gaechuserver.event.events;
 
-import java.time.LocalDateTime;
-
 public class LikeChangeEvent {
-	private Long referenceId;
+	private Long articleId;
 	private Long likerId;
-	private Long likeCount;
-	private LocalDateTime createdAt;
 	
-	public LikeChangeEvent(Long referenceId, Long likerId, Long likeCount) {
-		this.referenceId = referenceId;
+	public LikeChangeEvent(Long articleId, Long likerId) {
+		this.articleId = articleId;
 		this.likerId = likerId;
-		this.likeCount = likeCount;
-		this.createdAt = LocalDateTime.now();
 	}
 }
--- src/main/java/com/teambind/gaechuserver/event/publisher/LikeChangeEventPublisher.java
+++ src/main/java/com/teambind/gaechuserver/event/publisher/LikeChangeEventPublisher.java
@@ -1,11 +1,21 @@
 package com.teambind.gaechuserver.event.publisher;
 
-import com.teambind.gaechuserver.entity.Categories;
+import com.teambind.gaechuserver.event.events.LikeChangeEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@RequiredArgsConstructor
 public class LikeChangeEventPublisher {
+	
+	private final EventPublisher eventPublisher;
 	//TODO : implement this
-	public void publish(Categories categories, LikeChangeEventPublisher event) {
+	public void likeIncreaseEventPublish(LikeChangeEvent event) {
+		eventPublisher.publish("like-created", event);
+	}
 	
-	
+	public void likeDecreaseEventPublish(LikeChangeEvent event) {
+		eventPublisher.publish("like-deleted", event);
 	}
 }
